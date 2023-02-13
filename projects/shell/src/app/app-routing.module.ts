c07ecb19f5bb16c16272d026bdf24676e3214be8
web-dave <dave@webdave.de>
2023-02-13 19:14:50 +0100

    Step 6:
    Add routes

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [{ path: '**', component: NotFoundComponent }];
+const routes: Routes = [
+  {
+    path: 'books',
+    loadChildren: () => import('books/Module').then((m) => m.BooksModule),
+  },
+  {
+    path: 'todos',
+    loadChildren: () => import('todos/Module').then((m) => m.TodosModule),
+  },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
