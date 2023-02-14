129bc4f71e2be53f744b4f906cba6dc4b6146706
web-dave <dave@webdave.de>
2023-02-14 15:51:07 +0100

    Step 10:
    Load manifest on bootup

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,2 +1,6 @@
-import('./bootstrap')
-	.catch(err => console.error(err));
+import { loadManifest } from '@angular-architects/module-federation';
+
+loadManifest('/assets/mf.manifest.json')
+  .catch((err) => console.error(err))
+  .then((_) => import('./bootstrap'))
+  .catch((err) => console.error(err));
