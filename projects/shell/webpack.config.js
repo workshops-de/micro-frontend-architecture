f6be7bb5f09a6a7e53f688f1f8b92e52129c0b17
web-dave <dave@webdave.de>
2023-02-14 15:50:06 +0100

    Step 8:
    Remove remotes from Webpack.config

--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -4,11 +4,6 @@ const {
 } = require("@angular-architects/module-federation/webpack");
 
 module.exports = withModuleFederationPlugin({
-  remotes: {
-    books: "http://localhost:4201/remoteEntry.js",
-    todos: "http://localhost:4202/remoteEntry.js",
-  },
-
   shared: {
     ...shareAll({
       singleton: true,
