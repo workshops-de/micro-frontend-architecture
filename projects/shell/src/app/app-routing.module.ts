ec6b0acf4d14007cf7da2a345557794becc4a374
web-dave <dave@webdave.de>
2023-02-14 15:51:31 +0100

    Step 11:
    Fix Routes

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: 'books',
-    loadChildren: () => import('books/Module').then((m) => m.BooksModule),
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'manifest',
+        remoteName: 'books',
+        exposedModule: './Module',
+      }).then((m) => m.BooksModule),
   },
   {
     path: 'todos',
-    loadChildren: () => import('todos/Module').then((m) => m.TodosModule),
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'manifest',
+        remoteName: 'todos',
+        exposedModule: './Module',
+      }).then((m) => m.TodosModule),
   },
   { path: '**', component: NotFoundComponent },
 ];
